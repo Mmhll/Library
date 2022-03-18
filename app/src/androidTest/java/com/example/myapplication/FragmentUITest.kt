1c5cc74c73f99addc2668eb848c508b10a59bb0d
admin <admin@iMac-admin.local>
2022-03-18 16:15:24 +0500

    added ui test

--- app/src/androidTest/java/com/example/myapplication/FragmentUITest.kt
+++ app/src/androidTest/java/com/example/myapplication/FragmentUITest.kt
@@ -1,21 +1,38 @@
 package com.example.myapplication
 
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.EnumSet.allOf
 
 
 @RunWith(AndroidJUnit4::class)
 class FragmentUITest {
-    @Test fun testEventFragment(){
-        val scenario = launchFragmentInContainer<BooksFragment>()
-        onView(withId(R.id.recyclerBooks)).check(matches(isDisplayed()))
-    }
+
+    @get:Rule
+    val activity : ActivityTestRule<MainActivity> = ActivityTestRule<MainActivity>(MainActivity::class.java)
+
+    @Test
+    fun addFragmentDisplayedTest(){
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.buttonAdd)).perform(click())
+        onView(withId(R.id.add_fragment)).check(matches(isDisplayed()))
+     }
+
 }
